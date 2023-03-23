9e37332795de5a086a3e67c038b7e934e28c6d92
Krisna Dwi Nugraha <krisnadwinugrhaabe@gmail.com>
2023-03-23 11:44:15 +0700

    hapus komentar bawaan

--- resources/js/src/router/index.js
+++ resources/js/src/router/index.js
@@ -245,16 +245,13 @@ router.beforeEach((to, from, next) => {
 })
 
 router.beforeResolve((to, from, next) => {
-  // If this isn't an initial page load.
   if (to.name) {
-    // Start the route progress bar.
     NProgress.start()
   }
   next()
 })
 
 router.afterEach(() => {
-  // Complete the animation of the route progress bar.
   NProgress.done()
 })
 
